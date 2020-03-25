cc6e407f67d376c0e5a173cee154db2bb56f3cbc
pavel.savelev <paul.sav21@gmail.com>
2020-03-25 05:14:06 +0400

    Fix request mapping for controllers.

--- src/main/java/com/example/elastictest/controller/PdfDocumentController.java
+++ src/main/java/com/example/elastictest/controller/PdfDocumentController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/document")
+@RestController
+@RequestMapping("/document")
 public class PdfDocumentController {
 
     private PdfDocumentService pdfDocumentService;
--- src/main/java/com/example/elastictest/controller/TagController.java
+++ src/main/java/com/example/elastictest/controller/TagController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/tag")
+@RestController
+@RequestMapping("/tag")
 public class TagController {
 
     private TagService tagService;
